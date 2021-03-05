8ecce1c7fc1e1a95705f3c3f540e3c936a9a2954
Prateek Agarwal <pratekag@gmail.com>
2021-03-05 19:12:16 +0530

    Add constructors for client, server

--- src/main.cpp
+++ src/main.cpp
@@ -2,51 +2,126 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <random>
+#include <set>
 
-struct thread{
-        int status;
-};
-struct request
+enum class eventType
 {
-
-
+        ARRIVAL,
+        DEPARTURE,
+        TIMEOUT
 };
-enum class eventType {ARRIVAL, DEPARTURE, REQUESTTIMEOUT};
-struct serverQueue{
-  int capacity;
-  int currentLength;
-} ;
+
+enum class Status
+{
+        BUSY,
+        IDLE
+};
+
+struct queueObject{
+        int userId;
+        double arrivalTimeStamp;
+};
+
+struct metrics
+{
+        int timedOutRequests;
+        int successfulRequests;
+        double coreUtilization;
+        double requestDropRate;
+};
 struct server
 {
-        int numberThreads;
-        // vector <int> 
-        int currentThreadForRoundRobin;
-        serverQueue S;
-        int numRequestsDropped;
+        server(int numberThreads, double meanServiceTime,
+         double contextSwitchOverhead, int queueCapacity) : numberThreads{numberThreads}, 
+         meanServiceTime{meanServiceTime}, queueCapacity{queueCapacity},
+         contextSwitchOverhead{contextSwitchOverhead} {
+                 threads = new Status[numberThreads];
+                 for (auto i = 0 ; i <numberThreads; i+=1)
+                         threads[i] = Status::IDLE;
+                nextThread = 0;       
+         }
+         ~server(){
+                 delete threads;
+         }
 
-}Server;
-struct client{
-        double meanThinkTime;
-        int numberOfUsers;
+private:
+        int numberThreads;
+        int nextThread;
+        std::deque<queueObject> Q;
+        int numRequestsDropped;
+        Status *threads;
+        double meanServiceTime;
+        double contextSwitchOverhead;
+        const int queueCapacity;
+};
+struct client
+{
+
+        client(double meanThinkTime, int numberOfUsers, double meanTimeout) : meanThinkTime{meanThinkTime}, numberOfUsers{numberOfUsers}, meanTimeout{meanTimeout} {}
+        double getMeanThinktime()
+        {
+                return meanThinkTime;
+        }
+        double getNumberOfUsers()
+        {
+                return numberOfUsers;
+        }
+        double getMeanTimeout()
+        {
+                return meanTimeout;
+        }
+
+private:
+        const double meanThinkTime;
+        const int numberOfUsers;
         int numRequestsSent;
-}Client;
-struct Event{
+        const double meanTimeout;
+};
+struct Event
+{
         int typeOfEvent;
         float timeStamp;
-
+        int userId;
 };
-struct eventQueue{
+struct eventQueue
+{
         std::priority_queue<Event> pq;
-// this will be the priority queue in which both arrival and departure times are stored.
+        // this will be the priority queue in which both arrival and departure times are stored.
 };
-struct compareTimestamps{
-        bool operator() (Event const & e1, Event const & e2){
-                return e1.timeStamp> e2.timeStamp;
+struct compareTimestamps
+{
+        bool operator()(Event const &e1, Event const &e2)
+        {
+                return e1.timeStamp > e2.timeStamp;
         }
 };
 
-int main(){
+void arrive(){
 
+}
+void departure(){
 
+}
+void requestTimeout(){
+
+}
+// time is in seconds
+int main()
+{
+        int numRuns = 1;
+        int numberOfUsers = 100;
+        double meanThinkTime = 180.0;
+        int queueCapacity = 20;
+        int numThreads = 1;
+        double meanTimeout = 30.0;
+        double meanServiceTime = 0.3;
+        double contextSwitchOverhead = 0.1;
+        client C(meanThinkTime, numberOfUsers, meanTimeout);
+        std::cout << C.getMeanThinktime() << std::endl;
+        std::cout << C.getMeanTimeout() << std::endl;
+        std::cout << C.getNumberOfUsers() << std::endl;
+        server Server( int numberThreads, double meanServiceTime,
+         double contextSwitchOverhead, int queueCapacity);
         return 0;
 }
--- src/test.cpp
+++ src/test.cpp
@@ -1,39 +1,65 @@
 #include <queue>
 #include <iostream>
+#include <unordered_map>
 
-
-struct Event{
+struct Event
+{
 
         int typeOfEvent;
         float timeStamp;
-        Event(int typeOfEvent, float timeStamp):typeOfEvent{typeOfEvent}, timeStamp{timeStamp}{}
-
+        Event(int typeOfEvent, float timeStamp) : typeOfEvent{typeOfEvent}, timeStamp{timeStamp} {}
 };
-struct eventQueue{
+struct eventQueue
+{
         std::priority_queue<Event> pq;
 
-// this will be the priority queue in which both arrival and departure times are stored.
+        // this will be the priority queue in which both arrival and departure times are stored.
 };
-struct compareTimestamps{
-        bool operator() (Event const & e1, Event const & e2){
-                return e1.timeStamp> e2.timeStamp;
+struct compareTimestamps
+{
+        bool operator()(Event const &e1, Event const &e2)
+        {
+                return e1.timeStamp > e2.timeStamp;
         }
 };
 
-int main(){
+int main()
+{
 
-        std::priority_queue <Event,std::vector<Event>, compareTimestamps> pq;
+        std::priority_queue<Event, std::vector<Event>, compareTimestamps> pq;
         auto ts = 34.0;
         auto typeOfEvent = 1;
-        for (int i = 0; i < 10; i++){
+        for (int i = 0; i < 10; i++)
+        {
                 pq.push(Event(typeOfEvent, ts));
-                typeOfEvent = 1- typeOfEvent;
-                ts-=1;
+                typeOfEvent = 1 - typeOfEvent;
+                ts -= 1;
         }
-        for (int i = 0; i < 10; i+=1){
+        for (int i = 0; i < 10; i += 1)
+        {
                 auto e = pq.top();
                 pq.pop();
-                std::cout<<e.timeStamp<<e.typeOfEvent<<std::endl;
+                std::cout << e.timeStamp << e.typeOfEvent << std::endl;
+        }
+        std::unordered_map<uint64_t, uint64_t> *tmap;
+        tmap = new std::unordered_map<uint64_t, uint64_t>();
+        (*tmap)[1] = 2;
+        (*tmap)[2] = 3;
+        (*tmap)[4] = 7;
+        (*tmap)[8] = 2;
+        for (auto it = (*tmap).begin(); it != (*tmap).end(); ++it)
+        {
+                std::cout << it->first << " : " << it->second << std::endl;
+        }
+        std::cout << (*tmap).size() << std::endl;
+        (*tmap).clear();
+
+        std::cout << (*tmap).size() << std::endl;
+        std::cout << "hello world" << std::endl;
+
+        for (auto it = (*tmap).begin(); it != (*tmap).end(); ++it)
+        {
+                std::cout << it->first << " : " << it->second << std::endl;
         }
         return 0;
 }
