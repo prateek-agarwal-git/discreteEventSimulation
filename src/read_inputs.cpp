bb89c69e7c6e5f4029b8e6458dd3582827b4f8c2
Pooja Gayakwad <63789097+pooja314@users.noreply.github.com>
2021-03-07 16:18:21 +0530

    Added few more inputs
    Also added input.in

--- src/read_inputs.cpp
+++ src/read_inputs.cpp
@@ -38,37 +38,40 @@ int main()
 	
 	char distribution;
 	cin>>distribution;
-	cout<<"Disrtibution is "<<distribution<<endl;
 	
 	//use in case of exponential and normal distribution, as required.
-	// float mean; float sd; 
-	int S,E; // interval of uniform distribution
-	cin>>S>>E;
-	cout<<"next inputs are"<<S<<" "<<E<<" "<<endl;
+	float mean; // float sd; for normal distribution 
+	/*int S,E;  interval of uniform distribution 
+	cin>>S>>E; */
+	cin>>mean;
 	
-	unsigned int think_time, M, runs; //M is no. of users
-	cin>>M>>think_time;
-	cout<<"No of users:"<<M<<" think time="<<think_time<<endl;
+	unsigned int think_time, M; //M is no. of users
+	float request_timeout;
+	cin>>M>>think_time>>request_timeout;
+	
+	int threads_at_server, queue_cap;
+	float  context_switch_overhead;
+	cin>>threads_at_server>>queue_cap>>context_switch_overhead;
+	
+	int runs;
 	cin>>runs;
-	cout<<"No. of runs is:"<<runs<<endl;
-	
-	unsigned seed;
-	cin>>seed;
-	cout<<"Seed value is: "<<seed<<endl;
-	
-	
-	default_random_engine eg(seed); // default generator engine
-	
-	switch(distribution){
-	case 'E':
-		//expon(eg, (1/mean));
-		break;
-	case 'U':
-		uniform(eg, S, E);
-		break;
-	case 'N':
-		//normal(eg, mean, sd);
-		break;
+
+	unsigned seed;	
+	for(auto i=0;i<runs;i++)
+	{	cin>>seed;
+		default_random_engine eg(seed); // default generator engine
+		
+		switch(distribution){
+		case 'E':
+			expon(eg, (1/mean));
+			break;
+		case 'U':
+			//uniform(eg, S, E);
+			break;
+		case 'N':
+			//normal(eg, mean, sd);
+			break;
+		}
 	}
 	
 	
@@ -79,8 +82,9 @@ return 0;
 Input file pattern:
 Line 1: Distribution code ( 'E' for Exponetial, 'U' for uniform, 'N' for normal )
 Line 2: parameters for service time distribution(for uniform distribution [S, E]  , for Exponential distribution [mean], for normal distribution [mean, sd])
-Line 3: No_of_users, think_time
-Line 4: no. of runs(N)
+Line 3: No_of_users, think_time, request_time_out
+Line 4: Threads_at_server, queue_capacity, context_switch_overhead
+Line 5: no. of runs(N)
 Next N lines: seed values
 
 */
